31a3f267eb667714012d50d77bc80d53be48e159
aaronpp65 <aaron.pp@neuroplex.in>
2020-07-18 21:24:02 +0530

    initial commit

--- Backend/main.py
+++ Backend/main.py
@@ -1,1 +1,30 @@
 # development
+import cv2
+from maskdetector.mask_detector import MaskDetector
+
+## intialize objects 
+mask_detector =MaskDetector()
+
+def live_feed():
+    # load the video stream
+    cap= cv2.VideoCapture("/home/phi/Neuroplex/mask_detection/a.mp4")
+    while True:
+        # read the camera frame
+        success, frame = cap.read()
+        # if frame is not empty
+        if(success):
+            # pass frame to mask_detector and return labels and corresponding bounding boxes
+            labels,bboxes = mask_detector.detect(frame)
+            print(bboxes)
+            print(labels)
+            
+            if(len(labels)>0):
+                #if a person is  wearing mask emit true
+                if(labels[0]=="mask"):
+                    print("Person is wearing mask")
+                #if a person is not wearing mask emit flase
+                else:
+                    print("Person not wearing mask")
+                    
+if __name__ == '__main__':
+    live_feed()
