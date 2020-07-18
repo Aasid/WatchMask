3449224c9218cfd55f275f63903a919982a385b0
devdesignersid <dev.designer.sid@gmail.com>
2020-07-19 00:54:51 +0530

    Added webcam component

--- Frontend/src/js/components/CamComponent.jsx
+++ Frontend/src/js/components/CamComponent.jsx
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import Overlay from './Overlay';
+import Webcam from 'react-webcam';
+const videoConstraints = {
+	width: 1280,
+	height: 720,
+	facingMode: 'user',
+};
 
 class CamComponent extends Component {
+	constructor(props) {
+		super(props);
+		this.webcamRef = React.createRef(null);
+	}
+
 	render() {
 		return (
-			<div>
-				<h1>Cam</h1>
+			<div id="camComponent" className="camComponent">
+				<Webcam
+					audio={false}
+					height={720}
+					screenshotFormat="image/jpeg"
+					width={1280}
+					ref={this.webcamRef}
+					videoConstraints={videoConstraints}
+				/>
 				<Overlay />
 			</div>
 		);
